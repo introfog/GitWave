516a488772bab4b0cb1632c2187e5077415ea6b5
Dmitry Chubrick <chubrickdm@gmail.com>
2023-05-23 22:58:06 +0300

    Clean pom.xml file and fill it from the start

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.github.introfog.rgit {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.github.introfog.rgit to javafx.fxml;
-    exports com.github.introfog.rgit;
-}
